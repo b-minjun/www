43a747b1b29b239fb15e5ff5c34551aaae8c2a69
b-minjun <a01076555033@gmail.com>
2025-05-13 19:01:55 +0900

    login

--- login.php
+++ login.php
@@ -1,3 +1,16 @@
 <?php
-    include 'login.html';
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $conn = mysqli_connect("localhost", "root", "", "Cykor");
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $result = $conn->query($sql);
+    if($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+        echo "Welcome " . $row["username"];
+    } else {
+        echo "<script>alert('Invalid username or password');</script>";
+    }
 ?>
